3df5ce7ead8b1a420809bb839189bec67dbc23ec
ana beatriz dani bernardo <118557668+anabdani@users.noreply.github.com>
2024-01-08 19:28:51 -0300

    exercicios

---
+++ ex02.py
@@ -0,0 +1,2 @@
+nome = input('Qual seu nome? ')
+print(f'olá {nome}, é um prazer te conhecer!')
---
+++ ex03.py
@@ -0,0 +1,4 @@
+nome = input('Qual seu nome? ')
+salario = input('Qual seu salario? ')
+
+print(f'o funciona {nome} tem um salário de {salario} em junho')
