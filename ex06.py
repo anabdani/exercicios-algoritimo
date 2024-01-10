5f240d4e9c0224f8ba53ba1a034d46edf6669ddc
ana beatriz dani bernardo <118557668+anabdani@users.noreply.github.com>
2024-01-10 19:56:44 -0300

    aulas

---
+++ ex06.py
@@ -0,0 +1,4 @@
+numero = int(input('DIgite um número '))
+antecessor = numero -1
+sucessor = numero +1
+print(f'o número que você digitou é {numero},o seu antecessor é {antecessor},e o seu sucessor {sucessor}.')
---
+++ ex07.py
@@ -0,0 +1,4 @@
+numero = int(input('Digite um número: '))
+dobro = numero *2
+terca_parte = numero /3
+print(f'o dobro de {numero} é {dobro},A terça parte de {numero} é {terca_parte}.')
